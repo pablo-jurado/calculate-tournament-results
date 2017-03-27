a33b3b1f7d898bb2762ec52364fd8ef8139bd678
Pablo Jurado <pjurado85@gmail.com>
2017-03-26 21:20:44 -0500

    create get collection function

--- calculate-results.js
+++ calculate-results.js
@@ -1,11 +1,24 @@
 function calculateResultsArray (tournament) {
   // TODO: your code goes here
+
+  function getCollection (obj) {
+    var arr = []
+    for (var prop in obj) {
+      arr.push(obj[prop])
+    }
+    return arr
+  }
+
+  var games = getCollection(tournament.games)
+  var teams = getCollection(tournament.teams)
+
+  // console.log(games)
+  console.log(teams)
 }
 
 function calculateResultsObject (tournament) {
   // TODO: your code goes here
 }
-
 // Feel free to add additional functions to this file as needed.
 
 // this line is needed to export your functions so they can be used by the test suite
