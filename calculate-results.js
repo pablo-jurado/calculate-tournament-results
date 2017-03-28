21103e68f0c24154857ed470e816d67fb7df6b40
Pablo Jurado <pjurado85@gmail.com>
2017-03-28 12:17:45 -0500

    fix map callback for teams array

--- calculate-results.js
+++ calculate-results.js
@@ -1,16 +1,6 @@
 function calculateResultsArray (tournament) {
   // TODO: your code goes here
 
-  // function getData (obj) {
-  //   var arr = []
-  //   for (var prop in obj) {
-  //     arr.push({[prop]: obj[prop]})
-  //   }
-  //   return arr
-  // }
-  // var games = getData(tournament.games)
-  // var teams = getData(tournament.teams)
-
   function getWinners (game) {
     if (game.scoreA > game.scoreB) return { [game['teamA-id']]: game.scoreA }
     if (game.scoreB > game.scoreA) return { [game['teamB-id']]: game.scoreB }
@@ -27,15 +17,15 @@ function calculateResultsArray (tournament) {
   })
 
   var teamsArr = Object.keys(tournament.teams).map(function (item) {
-    return tournament.teams[item]
+    return { [item]: tournament.teams[item] }
   })
-  console.log(teamsArr)
+  // console.log(teamsArr)
   // console.log('hi!!!', gamesArr[0])
 
-  // var winners = games.map(getWinners)
-  // var loosers = games.map(getLoosers)
-  // console.log('winners!!!', winners)
-  // console.log('looser!', loosers)
+  var winners = gamesArr.map(getWinners)
+  var loosers = gamesArr.map(getLoosers)
+  console.log('winners!!!', winners)
+  console.log('looser!', loosers)
 
 } // <---- end of calculateResultsArray
 
