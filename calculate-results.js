196465d9a5bef63434ee59d79119499d3ee4356d
Pablo Jurado <pjurado85@gmail.com>
2017-03-30 20:35:05 -0500

    create points played function

--- calculate-results.js
+++ calculate-results.js
@@ -10,12 +10,22 @@ function calculateResultsArray (tournament) {
     }
     return arr
   }
+
+  function formatTeams (item) {
+    return {
+      'team-name': item.name,
+      'team-captain': item.captain,
+      'team-id': item['team-id']
+    }
+  }
+
   var teams = objToArray(tournament.teams, 'team-id')
+  teams = teams.map(formatTeams)
   var games = objToArray(tournament.games, 'game-id')
 
   teams.forEach(function (team) {
-    // reduce games array and return the numbers of games playes
-    team['gamesPlayed'] = games.reduce(function (all, item) {
+    // reduce games array and return the numbers of games played for each team
+    team['games-played'] = games.reduce(function (all, item) {
       if (item['status'] !== 'aborted') {
         if (item['teamB-id'] === team['team-id'] || item['teamA-id'] === team['team-id']) {
           all += 1
@@ -23,10 +33,62 @@ function calculateResultsArray (tournament) {
       }
       return all
     }, 0)
+
+    // check for games lost
+    team['games-lost'] = games.reduce(function (all, item) {
+      if (item['teamB-id'] === team['team-id']) {
+        if (item['scoreB'] < item['scoreA']) {
+          all += 1
+        }
+      } else if (item['teamA-id'] === team['team-id']) {
+        if (item['scoreB'] > item['scoreA']) {
+          all += 1
+        }
+      }
+      return all
+    }, 0)
+
+    // check for games won
+    team['games-won'] = games.reduce(function (all, item) {
+      if (item['teamB-id'] === team['team-id']) {
+        if (item['scoreB'] > item['scoreA']) {
+          all += 1
+        }
+      } else if (item['teamA-id'] === team['team-id']) {
+        if (item['scoreB'] < item['scoreA']) {
+          all += 1
+        }
+      }
+      return all
+    }, 0)
+
+    // check for tied games
+    team['games-tied'] = games.reduce(function (all, item) {
+      if (item['teamB-id'] === team['team-id'] || item['teamA-id'] === team['team-id']) {
+        if (item['scoreB'] === item['scoreA']) {
+          all += 1
+        }
+      }
+      return all
+    }, 0)
+
+    // get points played
+    team['points-played'] = games.reduce(function (all, item) {
+      if (item['teamB-id'] === team['team-id'] || item['teamA-id'] === team['team-id']) {
+        var result = item['scoreB'] + item['scoreA']
+        all += result
+      }
+      return all
+    }, 0)
+
   })
 
-  return teams
+  console.log(teams)
+  // return teams
 } // <---- end of calculateResultsArray
+
+
+
 
 function calculateResultsObject (tournament) {
   // TODO: your code goes here
@@ -35,6 +97,6 @@ function calculateResultsObject (tournament) {
 
 // this line is needed to export your functions so they can be used by the test suite
 module.exports = {
-  calculateResultsArray: calculateResultsArray,
-  calculateResultsObject: calculateResultsObject
+  calculateResultsArray: calculateResultsArray
+  // calculateResultsObject: calculateResultsObject
 }
