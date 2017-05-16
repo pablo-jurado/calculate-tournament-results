2fb54f3e712bf39f5e6affc40ffa66cc604cf5b4
Pablo Jurado <pjurado85@gmail.com>
2017-05-15 20:00:34 -0500

    started again, now working on reduce

--- calculate-results.js
+++ calculate-results.js
@@ -10,166 +10,29 @@ function objToArray (obj, key) {
   return arr
 }
 
-// change format from JSON to array result format
-function formatTeams (item) {
-  return {
-    'team-name': item.name,
-    'team-captain': item.captain,
-    'team-id': item['team-id']
-  }
-}
-// filter aborted games
-function filterAbortedGames (item) {
-  if (item.status !== 'aborted') return item
-}
-
-// filter teams that did not play
-function filterTeamsNotPlayed (item) {
-  if (item['games-played'] !== 0) return item
-}
-
-// calculate Victory points by the Swiss format
-function getSwissPoints (win, tie, diff) {
-  var points = 0
-  points = (win * 3000) + (tie * 1000) + diff
-  return points
-}
-
-// sort teams by swiss points
-function sortByPoints (a, b) {
-  return b['victory-points'] - a['victory-points']
-}
-
-// add place number (only used after sorting)
-function addPlace (element, index, array) {
-  element.place = index + 1
+function getFinishedGames (game) {
+  return (game['status'] !== 'aborted')
 }
 
 function calculateResultsArray (tournament) {
-  var teams = objToArray(tournament.teams, 'team-id').map(formatTeams)
-  var games = objToArray(tournament.games, 'game-id').filter(filterAbortedGames)
+  let arrTeams = objToArray(tournament.teams, 'team')
+  let arrGames = objToArray(tournament.games, 'game').filter(getFinishedGames)
 
-  // main forEach for reduce by team
-  teams.forEach(function (team) {
-    // reduce games array and return the numbers of games played for each team
-    team['games-played'] = games.reduce(function (all, item) {
-      if (item['teamB-id'] === team['team-id'] || item['teamA-id'] === team['team-id']) {
-        all += 1
-      }
-      return all
-    }, 0)
-
-    // check for lost games
-    function getGamesLost (all, item) {
-      if (item['teamB-id'] === team['team-id']) {
-        if (item['scoreB'] < item['scoreA']) all++
-      } else if (item['teamA-id'] === team['team-id']) {
-        if (item['scoreB'] > item['scoreA']) all++
-      }
-      return all
-    }
-    team['games-lost'] = games.reduce(getGamesLost, 0)
-
-    // check for games won
-    function getGamesWon (all, item) {
-      if (item['teamB-id'] === team['team-id']) {
-        if (item['scoreB'] > item['scoreA']) all++
-      } else if (item['teamA-id'] === team['team-id']) {
-        if (item['scoreB'] < item['scoreA']) all++
-      }
-      return all
-    }
-    team['games-won'] = games.reduce(getGamesWon, 0)
-
-    // check for tied games
-    function getGamesTied (all, item) {
-      if (item['teamB-id'] === team['team-id'] || item['teamA-id'] === team['team-id']) {
-        if (item['scoreB'] === item['scoreA']) all++
-      }
-      return all
-    }
-    team['games-tied'] = games.reduce(getGamesTied, 0)
-
-    // get points played
-    function getPointPLayed (all, item) {
-      if (item['teamB-id'] === team['team-id'] || item['teamA-id'] === team['team-id']) {
-        var result = item['scoreB'] + item['scoreA']
-        all += result
-      }
-      return all
-    }
-    team['points-played'] = games.reduce(getPointPLayed, 0)
-
-    // get points-won
-    function getPointsWon (all, item) {
-      if (item['teamB-id'] === team['team-id']) {
-        all += item['scoreB']
-      } else if (item['teamA-id'] === team['team-id']) {
-        all += item['scoreA']
-      }
-      return all
-    }
-    team['points-won'] = games.reduce(getPointsWon, 0)
-
-    // get point lost
-    team['points-lost'] = team['points-played'] - team['points-won']
-    // get point diff
-    team['points-diff'] = team['points-won'] - team['points-lost']
-    // get swiss points
-    team['victory-points'] = getSwissPoints(team['games-won'], team['games-tied'], team['points-diff'])
-  }) // <----- end of main forEach
-
-  teams = teams.filter(filterTeamsNotPlayed)
-
-  teams.sort(sortByPoints)
-
-  // add place number to each team
-  teams.forEach(addPlace)
-
-  return teams
-} // <---- end of calculateResultsArray
+  let result = arrGames.forEach((game) => {
+    let winner = ''
+    if (game['scoreB'] > game['scoreA']) winner = game['teamB-id']
+    if (game['scoreB'] < game['scoreA']) winner = game['teamA-id']
+    if (game['scoreB'] === game['scoreA']) winner = game['teamB-id'] + ' tied ' + game['teamA-id']
+    console.log(game.game, winner)
+  })
+}
 
 function calculateResultsObject (tournament) {
-  var teamsDataArr = calculateResultsArray(tournament)
 
-  function reformatObj (item) {
-    return {
-      gamesLost: item['games-lost'],
-      gamesPlayed: item['games-played'],
-      gamesTied: item['games-tied'],
-      gamesWon: item['games-won'],
-      place: item['place'],
-      pointDiff: item['points-diff'],
-      pointsLost: item['points-lost'],
-      pointsPlayed: item['points-played'],
-      pointsWon: item['points-won'],
-      teamCaptain: item['team-captain'],
-      teamName: item['team-name'],
-      victoryPoints: item['victory-points'],
-      teamId: item['team-id']
-    }
-  }
-
-  var reformatArr = teamsDataArr.map(reformatObj)
-
-  // convert arr to obj, asign team id as a obj key
-  function arrToObj (arr) {
-    var obj = {}
-    arr.forEach(function (item) {
-      var key = item['teamId']
-      delete item['teamId']
-      obj[key] = item
-    })
-    return obj
-  }
-
-  var teamsObj = arrToObj(reformatArr)
-
-  return teamsObj
 }
 
 // this line is needed to export your functions so they can be used by the test suite
-// module.exports = {
-//   calculateResultsArray: calculateResultsArray,
-//   calculateResultsObject: calculateResultsObject
-// }
+module.exports = {
+  calculateResultsArray: calculateResultsArray,
+  calculateResultsObject: calculateResultsObject
+}
--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-
-var finalResult = calculateResultsArray(tournament)
-
-console.log(finalResult[0]['games-played'])
-
-var data = finalResult.map(function (team) {
-  return team['games-played']
-})
-
-console.log(data)
-
-d3.select('.chart')
-  .selectAll('div')
-    .data(data)
-  .enter().append('div')
-    .style('width', function (d) { return x(d) + 'px' })
-    .text(function (d) { return d })
+//
+// var finalResult = calculateResultsArray(tournament)
+//
+// console.log(finalResult[0]['games-played'])
+//
+// var data = finalResult.map(function (team) {
+//   return team['games-played']
+// })
+//
+// console.log(data)
+//
+// d3.select('.chart')
+//   .selectAll('div')
+//     .data(data)
+//   .enter().append('div')
+//     .style('width', function (d) { return (d) *100 + 'px' })
+//     .text(function (d) { return d })
